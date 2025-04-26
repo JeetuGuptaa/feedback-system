699dece878d65c484ce61af0fa9ae207d119a5fa
JeetuGuptaa <jeetu270602@gmail.com>
2025-04-27 00:40:52 +0530

    frontend-init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Frontend is up and running!
+    </h1>
+    </>
+  )
+}
+
+export default App
