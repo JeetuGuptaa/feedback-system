db3c19134da1766fffbf61839746d878c0de3330
JeetuGuptaa <jeetu270602@gmail.com>
2025-04-27 14:34:09 +0530

    change backend base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://feedback-system-jpdo.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
